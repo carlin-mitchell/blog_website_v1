72bb122dba844952a2aaa0ad480d86a1e35f105c
Carlin Mitchell <carlin.d.mitchell@gmail.com>
2021-12-10 22:55:19 -0600

    style compose page, log post data in objects array

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const port = 3000;
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const { redirect } = require('express/lib/response');
 
 
 const app = express();
@@ -13,7 +14,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 
-// #################################### home "/" ######################################
+// #################################### "/" ###########################################
+const posts = []; console.log(posts);
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 
 app.get('/', (req, res) => {
@@ -41,8 +43,18 @@ app.get('/contact', (req, res) => {
 });
 
 // #################################### "/compose" ####################################
+app.get('/compose', (req, res) => {
+  res.render('compose')
+});
 
-
+app.post('/compose', (req, res) => {
+  const post = {
+    title: req.body.postTitle,
+    body: req.body.postBody
+  };
+  posts.push(post);
+  res.redirect('/');
+});
 
 // #################################### "/post" #######################################
 
